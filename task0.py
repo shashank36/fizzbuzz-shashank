e6531493f54379f7f5c393a68dcd45a53338dbdd
shashank <shashank.singh@quincus.com>
2023-02-01 18:41:56 -0400

    update fizbuzz code

--- task0.py
+++ task0.py
@@ -2,6 +2,8 @@ def fizzbuzz():
     for i in range(101):
         if i == 0:
             print(i)
+        elif i % 3 == 0 and  i % 5 ==0:
+            print("fizzbuzz")
         elif i % 3 == 0:
             print("Fizz")
         elif i % 5 == 0:
